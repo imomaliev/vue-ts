0e53d87337b0166837be5976a2a3b90e55885433
Sardorbek Imomaliev <imomaliev@hey.com>
2021-07-28 00:12:41 +0700

    install eslint-plugin-vue and vue-eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,16 @@ module.exports = {
   env: {
     node: true,
   },
-  parser: '@typescript-eslint/parser',
+  parser: 'vue-eslint-parser',
+  // https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
     'prettier',
   ],
 }
