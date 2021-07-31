07199e229d866f3e5aab4b18c262e5313e0b36ee
Sardorbek Imomaliev <imomaliev@hey.com>
2021-07-31 12:49:37 +0700

    install vue-jest and @vue/test-utils

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.vue$': 'vue3-jest',
+  },
+  moduleFileExtensions: ['json', 'js', 'jsx', 'ts', 'tsx', 'vue'],
 }
