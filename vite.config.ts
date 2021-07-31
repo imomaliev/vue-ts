e60e1ea8091aaaa8c2832e073a407b2806776019
Sardorbek Imomaliev <imomaliev@hey.com>
2021-07-31 21:55:10 +0700

    use vite base option only for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-ts/',
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
