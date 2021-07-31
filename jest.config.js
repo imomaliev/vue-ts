64f264f96f71fd639bc49a804e75522c7a041009
Sardorbek Imomaliev <imomaliev@hey.com>
2021-07-31 13:52:25 +0700

    fix: using wrong env for jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue3-jest',
   },
