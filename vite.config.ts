8d5ce8d3e7dff684ea03dfe2ba7844c22e0d00d7
Sardorbek Imomaliev <imomaliev@hey.com>
2021-07-31 21:46:51 +0700

    fix: vite not able to resolve @/

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/vue-ts/',
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
 })
